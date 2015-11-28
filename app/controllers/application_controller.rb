a8b30270621ac6dfa1c4553e51871ce9dd80dcbc
Brian Dunlap <briandunlap589@gmail.com>
2015-11-28 15:07:36 -0500

    Add index and create methods to tours controller and remove forgery safeguards from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+  before_action :json_defaults
+
+  rescue_from ActiveRecord::RecordNotFound do |error|
+    render json: { error: "Could not find object: #{error.message}" },
+      status: :not_found
+  end
+
+  def json_defaults
+    request.format = :json unless params[:format]
+  end
 end
