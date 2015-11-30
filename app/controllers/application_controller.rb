f210e5be15ebd0f076a69f5c161f7379da83d185
Brian Dunlap <briandunlap589@gmail.com>
2015-11-30 15:18:59 -0500

    Add current_user helper method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   before_action :json_defaults
+  helper_method :current_user, :authenticate_user!
 
   rescue_from ActiveRecord::RecordNotFound do |error|
     render json: { error: "Could not find object: #{error.message}" },
