2e28d81d8df4e912612994eed9743e5781f1bb98
Brian Dunlap <briandunlap589@gmail.com>
2015-11-30 11:56:52 -0500

    Make schema changes on master branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130153531) do
+ActiveRecord::Schema.define(version: 20151130160806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151130153531) do
     t.integer  "tour_id"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "category"
   end
 
   create_table "summaries", force: :cascade do |t|
@@ -36,10 +37,11 @@ ActiveRecord::Schema.define(version: 20151130153531) do
 
   create_table "tours", force: :cascade do |t|
     t.string   "title"
-    t.string   "length"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "distance"
+    t.string   "duration"
   end
 
   create_table "users", force: :cascade do |t|
