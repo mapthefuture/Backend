3775b10e200cbff547db7417edd28ee7f641ac9a
Brian Dunlap <briandunlap589@gmail.com>
2015-11-30 12:07:01 -0500

    Remove username from users table and add first name and last name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130165814) do
+ActiveRecord::Schema.define(version: 20151130170559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,11 +45,12 @@ ActiveRecord::Schema.define(version: 20151130165814) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
