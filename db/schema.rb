4a9bda14496b7b463024d4ce62fe8a29c231b186
Brian Dunlap <briandunlap589@gmail.com>
2015-11-30 15:21:45 -0500

    Add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130170559) do
+ActiveRecord::Schema.define(version: 20151130171511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20151130170559) do
     t.string   "password_digest"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "access_token"
   end
 
 end
