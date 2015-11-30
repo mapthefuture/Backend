e0f351a73979f0273febd31157c0b9e261dcad5f
Brian Dunlap <briandunlap589@gmail.com>
2015-11-30 10:57:59 -0500

    Change lat/lon data types from float to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128195249) do
+ActiveRecord::Schema.define(version: 20151130153531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "sites", force: :cascade do |t|
     t.string   "title"
-    t.float    "lat"
-    t.float    "decimal"
-    t.float    "lon"
+    t.decimal  "lat",        precision: 10, scale: 8
+    t.decimal  "lon",        precision: 10, scale: 8
     t.integer  "tour_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   create_table "summaries", force: :cascade do |t|
@@ -45,10 +44,10 @@ ActiveRecord::Schema.define(version: 20151128195249) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "password"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
