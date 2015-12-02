a5579c9a15c4050a627931c68fc7ddcf1cb97686
Brian Dunlap <briandunlap589@gmail.com>
2015-12-02 13:01:01 -0500

    Create ratings controller and making minor naming fixes

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,6 +1,6 @@
 class Tour < ActiveRecord::Base
   belongs_to :user
   has_many :sites
-  has_many :reviews, as: :reviewed
-  has_many :ratings, as: :rated
+  has_many :reviews, as: :reviewable
+  has_many :ratings, as: :rateable
 end
