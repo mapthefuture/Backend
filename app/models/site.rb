d0871916936d09b8d13e6344d0aca0ca63daab6a
Brian Dunlap <briandunlap589@gmail.com>
2015-12-08 20:25:30 -0500

    Temporarily remove audio validations for development

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,11 +3,15 @@ class Site < ActiveRecord::Base
   has_many :ratings, as: :rateable, dependent: :destroy
 
   has_attached_file :image 
-  # validates_attachment_file_name :image, matches: [/png\Z/, /jpg\Z/, /jpeg\Z/]
   has_attached_file :audio
 
-  do_not_validate_attachment_file_type :image
-  do_not_validate_attachment_file_type :audio
+  validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
+  # validates_attachment_content_type :image, content_type: /\Aimage/
+  # validates_attachment_content_type :audio,
+  # :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 
+  #                    'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 
+  #                    'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio',
+  #                    'audio/m4a' ]
 
   # geocoded_by :address   # can also be an IP address
   # after_validation :geocode          # auto-fetch coordinates
@@ -15,7 +19,7 @@ class Site < ActiveRecord::Base
   # reverse_geocoded_by :latitude, :longitude
   # after_validation :reverse_geocode  # auto-fetch address
 
-  def address
-  [self.street, self.city, self.state, self.zip, self.country].compact.join(', ')
-  end
+  # def address
+  # [self.street, self.city, self.state, self.zip, self.country].compact.join(', ')
+  # end
 end
