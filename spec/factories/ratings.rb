03e80be359b59f6d020f1404905d1cf1b2faea58
Brian Dunlap <briandunlap589@gmail.com>
2015-12-10 15:50:25 -0500

    Getting working tests for ratings

--- spec/factories/ratings.rb
+++ spec/factories/ratings.rb
@@ -1,14 +1,6 @@
 FactoryGirl.define do
-  factory :site_rating do
-    association :rateable, factory: :site
-    site
-    score     5
-    user
-  end
-
-  factory :tour_rating do
+  factory :rating do
     association :rateable, factory: :tour
-    tour
     score     5
     user
   end
