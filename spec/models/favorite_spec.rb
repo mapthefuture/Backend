32e0512ce0577a4543a50b9c8f1e297df0bc62de
Brian Dunlap <briandunlap589@gmail.com>
2015-12-10 15:52:43 -0500

    Add tests for favorite model

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:favorite)).to be_valid
+  end
+
+  context 'db' do
+    it { is_expected.to have_db_column(:user_id).of_type(:integer) }
+    it { is_expected.to have_db_column(:favoritable_id).of_type(:integer) }
+    it { is_expected.to have_db_column(:favoritable_type).of_type(:string) }
+  end
+
+  context 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:favoritable) }
+  end
 end
