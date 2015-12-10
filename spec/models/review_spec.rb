a7692563b4954fe9929f103d39a2a537c611ffa2
Brian Dunlap <briandunlap589@gmail.com>
2015-12-10 15:50:58 -0500

    Update review factory and get working model tests

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:review)).to be_valid
+  end
+
+  context 'db' do
+    it { is_expected.to have_db_column(:tagline).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_column(:body).of_type(:text) }
+    it { is_expected.to have_db_column(:user_id).of_type(:integer) }
+    it { is_expected.to have_db_column(:tour_id).of_type(:integer) }
+  end
+
+  context 'assocations' do
+    it {is_expected.to belong_to(:user) }
+    it {is_expected.to belong_to(:tour) }
+  end
 end
