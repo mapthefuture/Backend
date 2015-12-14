d1d1d8b8b9a9d5c7a031908dd47160c227cf600f
Brian Dunlap <briandunlap589@gmail.com>
2015-12-14 13:29:14 -0500

    Add geocoder config on tour model

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -4,4 +4,14 @@ class Tour < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   has_many :ratings, as: :rateable, dependent: :destroy
   has_many :favorites, as: :favoritable, dependent: :destroy
+
+  # geocoded_by :address   # can also be an IP address
+  # after_validation :geocode          # auto-fetch coordinates
+
+  reverse_geocoded_by :start_lat, :start_lon
+  after_validation :reverse_geocode  # auto-fetch address
+
+  # def address
+  # [self.street, self.city, self.state, self.zip, self.country].compact.join(', ')
+  # end
 end
