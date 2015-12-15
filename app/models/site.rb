048a9051aa0e35eae7e737cabbd147652a7881ef
Brian Dunlap <briandunlap589@gmail.com>
2015-12-15 12:01:56 -0500

    Add validators and helper methods for geocoder to tour model

--- app/models/site.rb
+++ app/models/site.rb
@@ -7,21 +7,4 @@ class Site < ActiveRecord::Base
 
   validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
   validates_attachment_file_name :audio, matches: [/mp3\Z/, /m4a\Z/]
-
-  # validates_attachment_content_type :image, content_type: /\Aimage/
-  # validates_attachment_content_type :audio,
-  # :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 
-  #                    'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 
-  #                    'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio',
-  #                    'audio/m4a' ]
-
-  # geocoded_by :address   # can also be an IP address
-  # after_validation :geocode          # auto-fetch coordinates
-
-  # reverse_geocoded_by :latitude, :longitude
-  # after_validation :reverse_geocode  # auto-fetch address
-
-  # def address
-  # [self.street, self.city, self.state, self.zip, self.country].compact.join(', ')
-  # end
 end
