4dfcccc355a02a7e9d222245e613063023e33892
Brian Dunlap <briandunlap589@gmail.com>
2015-12-15 16:37:31 -0500

    Add additional tests to registrations controller

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -2,96 +2,81 @@ require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
 
-  context 'POST create' do
-    it 'sends status created when successful' do
-      params = FactoryGirl.attributes_for(:user)
-      post :create, params
-      expect(response).to have_http_status(:created)
-    end
-
-    it 'sends status unprocessable when unsuccessful' do
-      params = nil
-      post :create, params
-      expect(response).to have_http_status(:unprocessable_entity)
-    end
-
-    it 'adds a user to the database' do
-      params = FactoryGirl.attributes_for(:user)
-      post :create, params
-      expect(User.count).to eq(1)
-    end
-
-    it 'will not create a user without a valid email' do
-      params = FactoryGirl.attributes_for(:user, email: nil)
-      post :create, params
-      expect(User.count).to eq(0)
-    end
-
-    it 'will not create a user without a password' do
-      params = FactoryGirl.attributes_for(:user, password: nil)
-      post :create, params
-      expect(User.count).to eq(0)
-    end
-
-    it 'requires users to have unique email addresses' do
-      params = FactoryGirl.attributes_for(:user, email: 'name@email.com')
-      post :create, params
-      expect(User.count).to eq(1)
-      post :create, params
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(User.count).to eq(1)
-    end
-
-    # it 'only accepts permitted params' do
-    #   params = { first_name: "John", last_name: "Doe", email: "name@email.com",
-    #              password: "password", favorite_animal: "blue whale" }
-    #   post :create, params
-    #   binding.pry
-    #   expect(response).to have_http_status(:unprocessable_entity)
-    # end
+  before(:each) do
+    allow(controller).to receive(:current_user).and_return(:user)
   end
 
-  context 'GET show' do
+    describe 'POST create' do
+
+      context 'validate successful requests'do 
+        let(:params) { FactoryGirl.attributes_for(:user) }
+
+        before(:each) do
+          post :create, user: params
+        end
+
+        it { expect(response).to have_http_status(:created) }
+        it { expect(User.count).to eq(1) }
+
+        it 'requires users to have unique email addresses' do
+          expect(User.count).to eq(1)
+          post :create, user: params
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(User.count).to eq(1)
+        end
+      end
+
+      context 'reject invalid requests' do
+        let(:params) { FactoryGirl.attributes_for(:user, email: nil) }
+
+        before(:each) do
+          post :create, user: params
+        end
+
+        it { expect(response).to have_http_status(:unprocessable_entity) }
+        it { expect(User.count).to eq(0) }
+      end
+    end
+
+  describe 'GET show' do
+
     let(:user) { FactoryGirl.create(:user) }
 
-    it 'accepts a valid email/password combination' do
-      params = { email: user.email, password: user.password }
-      get :show, params
-      expect(response).to have_http_status(:accepted)
+    context 'accept valid requests' do
+
+      before(:each) do 
+        get :show, email: user.email, password: user.password
+      end
+
+      it { expect(response).to have_http_status(:accepted) }
+      it { expect(response).to render_template('show.json.jbuilder') }
     end
 
-    it 'renders the jbuilder template when successful' do
-      params = { email: user.email, password: user.password }
-      get :show, params
-      expect(response).to render_template('show.json.jbuilder')
+    context 'reject invalid requests' do
+      
+      before(:each) do 
+        get :show, email: user.email, password: nil 
+      end
+
+      it { expect(response).to have_http_status(:unauthorized) }
     end
-
-    it 'rejects an invalid email/password combination' do
-      params = { email: user.email, password: nil }
-      get :show, params
-      expect(response).to have_http_status(:unauthorized)
-    end
-
-    # it 'renders json errors when unsuccessful' do
-    #   params = { email: user.email, password: nil }
-    #   get :show, params
-    #   binding.pry
-    #   expect(response).to include("error": "Invalid email or password.")
-    # end
-
   end
 
-  context 'PATCH update' do
-    let(:user) { FactoryGirl.create(:user) }
+  describe 'PATCH update' do
+    let(:params) do
+      { :first_name => 'Johnny', :email => 'Johnny@email.com' }
+    end
 
     before(:each) do
-      allow(controller).to receive(:current_user).and_return(user)
+      @user = create(:user)
+      allow(controller).to receive(:current_user).and_return(@user)
+      patch :update, id: @user, user: params
+      @user.reload
     end
 
-    it 'sends status accepted when user is updated successfully' do
-      params = FactoryGirl.attributes_for(:user)
-      patch :update, params
-      expect(response).to have_http_status(:accepted)
-    end
+    it { expect(@user.first_name).to eq(params[:first_name]) }
+    it { expect(@user.email).to eq(params[:email]) }
+    it { expect(response).to have_http_status(:accepted) }
+    it { expect(response).to render_template('update.json.jbuilder') }
   end
 end
--- spec/factories/tours.rb
+++ spec/factories/tours.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :tour do
     title        "Walking Tour of Atlanta"
     description  "A full tour of the city"
-    distance     5.5
+    length     5.5
     duration     50000
     start_lat    12.121212
     start_lon    34.343434
